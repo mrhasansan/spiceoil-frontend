d33b777ccc8455111f6b50c4906af4ccec001dba
Hasansan <marufhasansan@gmail.com>
2024-07-17 21:12:39 +0700

    product card rev

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { useLoaderData } from "react-router-dom";
 
 export async function loader() {
   const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/products`);
@@ -10,6 +11,7 @@ export async function loader() {
 }
 
 export function ProductCard() {
+  const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
   return (
     <div>
       <Card className="w-[350px]">
@@ -20,6 +22,7 @@ export function ProductCard() {
         <CardContent>
           <img src="https://m.media-amazon.com/images/I/81DHmsc+f5L.jpg" alt="" />
           <p>Price</p>
+          <pre> {JSON.stringify(products)}</pre>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline">Add To Chart</Button>
